825e78facb4dff27db3ff2ccf577cc14104f8650
shu20031026 <shu20031026@gmail.com>
2022-02-17 18:29:43 +0900

    api側のprotoファイルの生成

--- generate_code.sh
+++ generate_code.sh
@@ -2,4 +2,5 @@
 
 protoc proto/helloworld.proto \
     --js_out=import_style=commonjs:cliant/helloworld \
-    --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:cliant/helloworld
+    --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:cliant/helloworld \
+    --go-grpc_out=api/helloworld
